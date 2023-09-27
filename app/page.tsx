f1c24ff51553a9789c9caecede9728fb8a26d547
k74 <kashhh4u@gmail.com>
2023-09-27 19:49:59 +0100

    Add shipping

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import hero_image from "../public/hero_image.jpg"
 import cart from "../public/cart.svg"
 import family from "../public/family.svg"
 import star from "../public/star.svg"
+import shipping from "../public/shipping.svg"
 import Card from "./components/Card"
 
 export default function Home() {
@@ -21,10 +22,18 @@ export default function Home() {
         <p>Take your taste buds through a sophisticated chocolate experience.</p>
         <button className={styles.button}>Find yours</button>
         <div className={styles.cards_container}>
-          <Card heading="2 million cases sold" paragraph="Unlocking the full power of desserts, since 2010." img={cart.src} alt="Shopping cart"></Card>
+          <Card heading="2 million desserts sold" paragraph="Unlocking the full power of desserts, since 2010." img={cart.src} alt="Shopping cart"></Card>
           <Card heading="100k 5-star reviews" paragraph="Quality speaks for itself, and our reviews do too." img={star.src} alt="Shopping cart"></Card>
           <Card heading="Family-owned & operated" paragraph="Great quality and insight comes from working with the people we know best." img={family.src} alt="Shopping cart"></Card>
         </div>
+        <div className={styles.shipping}>
+          <img src={shipping.src} alt="Shipping" />
+          <div>
+            <h2>Get fast, free delivery with Buy with Prime.</h2>
+            <p>Use your Prime shopping benefits directly on our site.</p>
+          </div>
+        </div>
+        <button className={styles.button}>Shop All</button>
       </section>
     </main>
   )
