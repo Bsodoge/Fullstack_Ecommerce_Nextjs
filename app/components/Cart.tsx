f2f4a09ff2d62bf53fa309fc4de8c030ded68293
k74 <kashhh4u@gmail.com>
2023-10-05 07:05:46 +0100

    Structure card

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -1,13 +1,30 @@
+'use client'
+
 import styles from "./Cart.module.css"
 
 interface props{
-    showCart: boolean
+    showCart: boolean,
+    setShowCart: Function
 }
 
-export default function Cart({showCart} : props){
+export default function Cart({showCart, setShowCart} : props){
+    const closeCart = () => {
+        setShowCart(false);
+    }
     return(
         <div className={`${styles.cart_container} ${ showCart && styles.active}`}>
+            <div className={styles.close_container}>
+                <span className={styles.close} onClick={e => closeCart()}>x</span>
+            </div>
+            <h1>Your Cart</h1>
+            <div className={styles.cart_item}>
 
+            </div>
+            <div className={styles.price_container}>
+                <p>Subtotal:</p>
+                <p className={styles.price}>${}</p>
+            </div>
+            <button>Checkout</button>
         </div>
     )
 }
