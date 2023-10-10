430c18942a7fcb7e79ad57635f613b9d0b39bd69
k74 <kashhh4u@gmail.com>
2023-10-10 18:43:45 +0100

    Create shopping cart context

---
+++ app/context/shoppingCartContext.tsx
@@ -0,0 +1,19 @@
+import { useContext, createContext, ReactNode } from "react";
+
+const ShoppingCartContext = createContext({});
+
+interface props {
+    children: ReactNode
+}
+
+export function ShoppingCartProvider({children} : props){
+    return(
+        <ShoppingCartContext.Provider value={{}}>
+            {children}
+        </ShoppingCartContext.Provider>
+    )
+}
+
+export function useShoppingCart(){
+    return useContext(ShoppingCartContext);
+}
