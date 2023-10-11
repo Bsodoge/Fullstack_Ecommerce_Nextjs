76a9ee67f4aed2ab51c1843c23a904d64298b0ac
k74 <kashhh4u@gmail.com>
2023-10-11 19:26:04 +0100

    Add quantity

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,9 +4,11 @@ import Link from "next/link"
 import styles from "./Header.module.css"
 import Cart from "./Cart"
 import { useState } from "react"
+import { useShoppingCart } from "../context/shoppingCartContext"
 
 export default function Header() {
     const [showCart, setShowCart] = useState<boolean>(false);
+    const { cartQuantity } = useShoppingCart();
     return (
         <header className={styles.header}>
             <div className={styles.logo}>
@@ -14,7 +16,7 @@ export default function Header() {
             </div>
             <div className={styles.buttons}>
                 <Link href="/"><button className={styles.button}>About</button></Link>
-                <Link href="/products"><button className={styles.button}>Shop</button></Link>
+                <Link href="/products"><button className={styles.button}>Shop ({cartQuantity})</button></Link>
                 <div className={styles.wrapper}><button className={styles.button} onClick={e => setShowCart(showcart => !showcart)}>Cart</button></div>
                 <div className="search"></div>
             </div>
