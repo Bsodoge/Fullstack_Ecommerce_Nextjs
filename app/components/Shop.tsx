b9c317ded74a835969e2e46481c4fa5d89b74903
k74 <kashhh4u@gmail.com>
2023-10-15 20:12:33 +0100

    Get data and map

--- app/components/Shop.tsx
+++ app/components/Shop.tsx
@@ -3,9 +3,21 @@
 import styles from "./Shop.module.css"
 import ProductCard from "./ProductCard"
 import { useQueryState } from 'next-usequerystate'
+import { useEffect, useState } from "react";
+
+interface product{
+    id: number,
+    product_name: string,
+    product_image: string,
+    product_price: number
+}
 
 export default function Shop() {
-    const [category, setCategory] = useQueryState('category')
+    const [category, setCategory] = useQueryState('category');
+    const [products, setProducts] = useState<product[]>([]);
+    useEffect(() => {
+        fetch('/api/getData').then(response => response.json()).then(data => setProducts(data));
+    }, [])
     return (
         <div className={styles.shop_container}>
             <h1>{category}</h1>
@@ -14,17 +26,16 @@ export default function Shop() {
                     <h2>Shop</h2>
                     <p><strong>Selecting a filter option will automatically update results</strong></p>
                     <ul className={styles.option_list}>
-                        <li><label className={styles.option} onClick={e => setCategory('lorem')}><input type="radio" name="option"/>Lorem</label></li>
-                        <li><label className={styles.option} onClick={e => setCategory('ipsum')}><input type="radio" name="option"/>Ipsum</label></li>
-                        <li><label className={styles.option} onClick={e => setCategory('dolor')}><input type="radio" name="option"/>Dolor</label></li>
+                        <li><label className={styles.option} onClick={e => setCategory('lorem')}><input type="radio" name="option" />Lorem</label></li>
+                        <li><label className={styles.option} onClick={e => setCategory('ipsum')}><input type="radio" name="option" />Ipsum</label></li>
+                        <li><label className={styles.option} onClick={e => setCategory('dolor')}><input type="radio" name="option" />Dolor</label></li>
                     </ul>
                 </div>
                 <div className={styles.product_container}>
-                    <ProductCard id={1} img="" alt="img" productName="test" price={2} />
-                    <ProductCard id={2} img="" alt="img" productName="test" price={2} />
-                    <ProductCard id={3} img="" alt="img" productName="test" price={2} />
-                    <ProductCard id={4} img="" alt="img" productName="test" price={2} />
-                    <ProductCard id={5} img="" alt="img" productName="test" price={2} />
+                    {
+                        products.map(product => <ProductCard id={product.id} img="" alt="img" productName={product.product_name} price={product.product_price} />
+                        )
+                    }
                 </div>
             </div>
         </div>
