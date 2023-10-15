2b21fef1064eb67cbd220f7400e66622a1621c5a
k74 <kashhh4u@gmail.com>
2023-10-15 10:56:55 +0100

    Update config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    rules: {
-        "@next/next/no-img-element": "off",
-    },
+    eslint: { ignoreDuringBuilds: true },
 }
 
 module.exports = nextConfig
