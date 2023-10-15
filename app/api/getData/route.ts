a84b2fb9ad2c61b20243ce72c7395d5e69c82c1c
k74 <kashhh4u@gmail.com>
2023-10-15 20:12:22 +0100

    Update route

--- app/api/getData/route.ts
+++ app/api/getData/route.ts
@@ -3,8 +3,9 @@ import { NextResponse } from 'next/server';
  
 export async function GET(request: Request) {
   try {
-    const products = await sql`SELECT * FROM Products;`;
-    return NextResponse.json({ products }, { status: 200 });
+    const products  = await sql`SELECT * FROM Products;`;
+    const rows = products.rows;
+    return NextResponse.json(rows, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   } 
