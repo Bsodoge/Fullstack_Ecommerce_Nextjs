60139954389278f838645eeb13f5f0afe338155f
k74 <kashhh4u@gmail.com>
2023-10-17 18:04:12 +0100

    Add new route

---
+++ app/api/getSpecificProduct/route.ts
@@ -0,0 +1,12 @@
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+
+export async function POST(request: Request) {
+  try {
+    const itemID = await request.json();
+    const { rows } = await sql`SELECT * FRoM products WHERE id = ${itemID};`;
+    return NextResponse.json(rows[0], { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
