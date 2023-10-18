81b7572261e1b2dfde5bf8c7f4fc85d5b96c645c
k74 <kashhh4u@gmail.com>
2023-10-18 13:58:17 +0100

    Updata API route

--- app/api/getData/route.ts
+++ app/api/getData/route.ts
@@ -1,12 +1,11 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
- 
+
 export async function GET(request: Request) {
   try {
-    const products  = await sql`SELECT * FROM Products;`;
-    const rows = products.rows;
+    const { rows } = await sql`SELECT * FRoM products;`;
     return NextResponse.json(rows, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
-  } 
+  }
 }
--- app/api/getSpecificProduct/route.ts
+++ app/api/getSpecificProduct/route.ts
@@ -5,6 +5,7 @@ export async function POST(request: Request) {
   try {
     const itemID = await request.json();
     const { rows } = await sql`SELECT * FRoM products WHERE id = ${itemID};`;
+    console.log(rows);
     return NextResponse.json(rows[0], { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
