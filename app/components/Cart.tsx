95a3d916a6b65c9cfdcc7088a5c37b26d2221a95
k74 <kashhh4u@gmail.com>
2023-10-18 14:02:11 +0100

    Update Cart

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -1,12 +1,26 @@
 'use client'
 
+import { useEffect, useState } from "react";
 import { useShoppingCart } from "../context/shoppingCartContext";
 import styles from "./Cart.module.css"
 import CartItem from "./CartItem";
 
+interface IProduct {
+    id: number,
+    product_name: string,
+    product_image: string,
+    product_price: number
+}
+
 
 export default function Cart() {
     const { setShowCart, showCart, cartItems } = useShoppingCart();
+    const [products, setProducts] = useState<IProduct[]>([]);
+    const formatter = new Intl.NumberFormat("en-gb", {currency: "GBP",  style: "currency"})
+    useEffect(() => {
+        fetch('/api/getData').then(response => response.json()).then(data => setProducts(data));
+    }, [])
+
     const closeCart = () => {
         setShowCart(false);
     }
@@ -23,7 +37,7 @@ export default function Cart() {
             </div>
             <div className={styles.price_container}>
                 <p>Subtotal:</p>
-                <p className={styles.price}>${ }</p>
+                <p className={styles.price}>{formatter.format(cartItems.reduce((total, item) => total + (item.quantity * products.find(product => product.id === item.id)?.product_price!), 0))}</p>
             </div>
             <button className={styles.button}>Checkout</button>
         </div>
