9b078bd8a61ea1227da1afa1e57b4dea92da02f2
k74 <kashhh4u@gmail.com>
2023-10-18 17:07:12 +0100

    Add filter by category

--- app/components/Shop.tsx
+++ app/components/Shop.tsx
@@ -4,13 +4,7 @@ import styles from "./Shop.module.css"
 import ProductCard from "./ProductCard"
 import { useQueryState } from 'next-usequerystate'
 import { useEffect, useState } from "react";
-
-interface IProduct{
-    id: number,
-    product_name: string,
-    product_image: string,
-    product_price: number
-}
+import { IProduct } from "../interfaces/IProduct";
 
 export default function Shop() {
     const [category, setCategory] = useQueryState('category');
@@ -20,20 +14,24 @@ export default function Shop() {
     }, [])
     return (
         <div className={styles.shop_container}>
-            <h1>{category}</h1>
+            <h1>{category || 'All'}</h1>
             <div className={styles.grid}>
                 <div className={styles.options}>
                     <h2>Shop</h2>
                     <p><strong>Selecting a filter option will automatically update results</strong></p>
                     <ul className={styles.option_list}>
-                        <li><label className={styles.option} onClick={e => setCategory('lorem')}><input type="radio" name="option" />Lorem</label></li>
-                        <li><label className={styles.option} onClick={e => setCategory('ipsum')}><input type="radio" name="option" />Ipsum</label></li>
-                        <li><label className={styles.option} onClick={e => setCategory('dolor')}><input type="radio" name="option" />Dolor</label></li>
+                        <li><label className={styles.option} onClick={e => setCategory('All')}><input type="radio" name="option" checked={category === 'All' ? true : false} />All</label></li>
+                        <li><label className={styles.option} onClick={e => setCategory('Cake')}><input type="radio" name="option" checked={category === 'Cake' ? true : false} />Cakes</label></li>
+                        <li><label className={styles.option} onClick={e => setCategory('Cookies')}><input type="radio" name="option" checked={category === 'Cookies' ? true : false} />Cookies</label></li>
+                        <li><label className={styles.option} onClick={e => setCategory('Confectionery')}><input type="radio" name="option" checked={category === 'Confectionery' ? true : false} />Confectioneries</label></li>
                     </ul>
                 </div>
                 <div className={styles.product_container}>
                     {
+                        category === 'All' ? 
                         products.map(product => <ProductCard key={product.id} id={product.id} img="" alt="img" productName={product.product_name} price={product.product_price} />)
+                        :
+                        products.filter(product => product.product_category === category).map(product => <ProductCard key={product.id} id={product.id} img="" alt="img" productName={product.product_name} price={product.product_price} />)
                     }
                 </div>
             </div>
