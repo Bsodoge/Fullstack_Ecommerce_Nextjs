110fa5f158dd97f8ee64435ee3e2dd4f4fe32b52
k74 <kashhh4u@gmail.com>
2023-10-30 06:32:04 +0000

    Add address validation interface

---
+++ app/interfaces/IAddressValidation.tsx
@@ -0,0 +1,11 @@
+export interface IAddressValidation {
+    email: string,
+    country: string,
+    firstName: string,
+    lastName: string,
+    address: string,
+    apartment: string,
+    city: string,
+    county: string,
+    postcode: string
+}
