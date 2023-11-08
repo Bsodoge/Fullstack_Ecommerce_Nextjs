30098c75b6dd9b02246710015aa74fe0e8b2ca96
k74 <kashhh4u@gmail.com>
2023-11-08 07:03:47 +0000

    Create basic layout for login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,16 @@
 import styles from "./page.module.css"
-export default function Login(){
-    return(
+export default function Login() {
+    return (
         <main className={styles.main}>
+            <form action="">
+                <h1>Sign in</h1>
+                <div className={styles.field}>
+                    <input type="text" name="email" id="" placeholder="Email" required />
+                </div>
+                <button className={styles.signin_button} >Continue</button>
+            </form>
+            <span>New to next?</span>
+            <button className={styles.signup_button}>Create your new next account</button>
         </main>
     )
 }
