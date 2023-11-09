c64bd2c822e49eaefe6f261ce754906480954b29
k74 <kashhh4u@gmail.com>
2023-11-09 06:50:38 +0000

    Add basic login functionality

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,42 @@
-import styles from "./page.module.css"
+'use client'
+
+import { FormEvent, useRef, useState } from "react";
+import styles from "./page.module.css";
+import { useRouter } from "next/navigation";
 export default function Login() {
+    const usernameInput = useRef<HTMLInputElement>(null);
+    const passwordInput = useRef<HTMLInputElement>(null);
+    const [error, setError] = useState<boolean>(false);
+    const route = useRouter();
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+        if (!usernameInput.current?.value.trim().length || !passwordInput.current?.value.trim().length) {
+            setError(true);
+            return;
+        }
+        const options: RequestInit = {
+            method: 'POST',
+            body: JSON.stringify({ username: usernameInput.current?.value, password: passwordInput.current?.value })
+        }
+        const response = await fetch('/api/auth/login', options);
+        const data = await response.json();
+        if (!data.authenticated) {
+            setError(true);
+        } else {
+            setError(false);
+            route.push('/checkout');
+        }
+    }
     return (
         <main className={styles.main}>
-            <form action="">
+            <form action="" className={styles.form} onSubmit={handleSubmit}>
                 <h1>Sign in</h1>
+                {error ? <span className={styles.error}>Username or password is incorrect</span> : <></>}
                 <div className={styles.field}>
-                    <input type="text" name="email" id="" placeholder="Email" required />
+                    <input type="text" name="username" id="" placeholder="Username" ref={usernameInput} required />
+                </div>
+                <div className={styles.field}>
+                    <input type="password" name="passowrd" id="" placeholder="Password" ref={passwordInput} required />
                 </div>
                 <button className={styles.signin_button} >Continue</button>
             </form>
