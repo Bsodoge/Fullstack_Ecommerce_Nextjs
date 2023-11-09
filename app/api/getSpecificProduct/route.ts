f1aac1918b6cb78c184bb4f0262fe6a3ac1f405a
k74 <kashhh4u@gmail.com>
2023-11-09 06:58:00 +0000

    Update specific product route

--- app/api/getSpecificProduct/route.ts
+++ app/api/getSpecificProduct/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 export async function POST(request: Request) {
   try {
     const itemID = await request.json();
-    const { rows } = await sql`SELECT * FRoM products WHERE id = ${itemID};`;
+    const { rows } = await sql`SELECT * FROm products WHERE id = ${itemID};`;
     console.log(rows);
     return NextResponse.json(rows[0], { status: 200 });
   } catch (error) {
