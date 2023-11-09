dca6247e6aa9375f9d483abb8f64b966644eda1c
k74 <kashhh4u@gmail.com>
2023-11-09 06:55:19 +0000

    Update cart

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { useShoppingCart } from "../context/shoppingCartContext";
 import styles from "./Cart.module.css"
 import CartItem from "./CartItem";
+import Link from "next/link";
 
 interface IProduct {
     id: number,
@@ -16,7 +17,7 @@ interface IProduct {
 export default function Cart() {
     const { setShowCart, showCart, cartItems } = useShoppingCart();
     const [products, setProducts] = useState<IProduct[]>([]);
-    const formatter = new Intl.NumberFormat("en-gb", {currency: "GBP",  style: "currency"})
+    const formatter = new Intl.NumberFormat("en-gb", { currency: "GBP", style: "currency" })
     useEffect(() => {
         fetch('/api/getData').then(response => response.json()).then(data => setProducts(data));
     }, [])
@@ -29,17 +30,31 @@ export default function Cart() {
             <div className={styles.close_container}>
                 <span className={styles.close} onClick={e => closeCart()}>x</span>
             </div>
-            <h1>Your Cart</h1>
-            <div className={styles.cart_items}>
-                {
-                    cartItems.map(item => <CartItem key={item.id} id={item.id} quantity={item.quantity} />)
-                }
-            </div>
-            <div className={styles.price_container}>
-                <p>Subtotal:</p>
-                <p className={styles.price}>{formatter.format(cartItems.reduce((total, item) => total + (item.quantity * products.find(product => product.id === item.id)?.product_price!), 0))}</p>
-            </div>
-            <button className={styles.button}>Checkout</button>
+            {
+                cartItems.length ?
+                    <div>
+                        <h1>Your Cart</h1>
+                        <div className={styles.cart_items}>
+                            {
+                                cartItems.map(item => <CartItem key={item.id} id={item.id} quantity={item.quantity} />)
+                            }
+                        </div>
+                        <div className={styles.price_container}>
+                            <p>Subtotal:</p>
+                            <p className={styles.price}>{formatter.format(cartItems.reduce((total, item) => total + (item.quantity * products.find(product => product.id === item.id)?.product_price!), 0))}</p>
+                        </div>
+                        <Link href="/checkout"><button className={styles.button}>Checkout</button></Link>
+                    </div>
+                    : 
+                    <div className={styles.empty}>
+                        <div className={styles.text}>
+                            <h1>Let's add something.</h1>
+                            <p>Ready to find the dessert for you?</p>
+                        </div>
+                        <Link href="/products?category=All"><button className={styles.shopnow_button}>Shop Now</button></Link>
+                    </div>
+                
+            }
         </div>
     )
 }
