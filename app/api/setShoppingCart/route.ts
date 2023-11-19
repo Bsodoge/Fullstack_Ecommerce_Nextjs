4b3d66cfaa99e4f6c3b3ea2ac3a2348da0c253e2
k74 <kashhh4u@gmail.com>
2023-11-19 18:17:02 +0000

    Convert array to string

--- app/api/setShoppingCart/route.ts
+++ app/api/setShoppingCart/route.ts
@@ -4,7 +4,8 @@ import { NextResponse } from 'next/server';
 export async function POST(request: Request) {
   try {
     const {shoppingCart, userID} = await request.json();
-    const { rows } = await sql`INSERT INTO shoppingcarts (shoppingcart, user_id ) VALUES (${shoppingCart}, ${userID});`;
+    const stringCart = shoppingCart.map((cartItem : object) => JSON.stringify(cartItem)).toString();
+    const { rows } = await sql`INSERT INTO shoppingcarts (shoppingcart, user_id ) VALUES (${stringCart}, ${userID});`;
     return NextResponse.json(rows[0], { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
