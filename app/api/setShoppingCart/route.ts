58d4f3cf63fbe6c9c5327c2e6c65c0875d4b828c
k74 <kashhh4u@gmail.com>
2023-11-20 17:38:58 +0000

    Fix undefined bug

--- app/api/setShoppingCart/route.ts
+++ app/api/setShoppingCart/route.ts
@@ -4,10 +4,11 @@ import { NextResponse } from 'next/server';
 export async function POST(request: Request) {
   try {
     const {shoppingCart, userID} = await request.json();
-    const stringCart = shoppingCart.map((cartItem : object) => JSON.stringify(cartItem)).toString();
+    const stringCart = shoppingCart.map((cartItem : any) => JSON.stringify(cartItem)).toString();
     const { rows } = await sql`INSERT INTO shoppingcarts (shoppingcart, user_id ) VALUES (${stringCart}, ${userID});`;
-    return NextResponse.json(rows[0], { status: 200 });
+    return NextResponse.json(rows, { status: 200 });
   } catch (error) {
+    console.log(error)
     return NextResponse.json({ error }, { status: 500 });
   }
 }
