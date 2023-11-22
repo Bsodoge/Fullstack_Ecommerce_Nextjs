31c7af5390f786600e3568d856c238996ca0373d
k74 <kashhh4u@gmail.com>
2023-11-22 19:25:44 +0000

    Make it so shopping cart updates in db

--- app/api/setShoppingCart/route.ts
+++ app/api/setShoppingCart/route.ts
@@ -5,7 +5,7 @@ export async function POST(request: Request) {
   try {
     const {shoppingCart, userID} = await request.json();
     const stringCart = shoppingCart.map((cartItem : any) => JSON.stringify(cartItem)).toString();
-    const { rows } = await sql`INSERT INTO shoppingcarts (shoppingcart, user_id ) VALUES (${stringCart}, ${userID});`;
+    const { rows } = await sql`INSERT INTO shoppingcarts (shoppingcart, user_id ) VALUES (${stringCart}, ${userID}) ON CONFLICT (user_id) DO UPDATE SET shoppingcart=${stringCart};`;
     return NextResponse.json(rows, { status: 200 });
   } catch (error) {
     console.log(error)
