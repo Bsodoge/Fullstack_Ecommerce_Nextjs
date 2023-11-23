5739ef7fb16d477468579ca343969f7d222d3fba
k74 <kashhh4u@gmail.com>
2023-11-23 19:51:51 +0000

    Make route work

--- app/api/getShoppingCart/route.ts
+++ app/api/getShoppingCart/route.ts
@@ -4,8 +4,10 @@ import { NextResponse } from 'next/server';
 export async function POST(request: Request) {
   try {
     const userID = await request.json();
+    console.log(userID)
     const { rows } = await sql`SELECT * FROM shoppingcarts WHERE user_id = ${userID};`;
-    return NextResponse.json(rows[0], { status: 200 });
+
+    return NextResponse.json(rows[0].shoppingcart, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
